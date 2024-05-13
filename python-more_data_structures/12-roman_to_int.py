cec7f90534da956ed56a5194cc67de5f9786a9ce
Ntombikayise Kidibone Makola <ntombikayise.makola@gmail.com>
2024-05-13 10:59:24 +0000

    12. Roman to Integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string: str):
-    if roman_string is None or type(roman_string) != str:
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    numbers = [data[x] for x in roman_string] + [0]
-    rep = 0
-
-    for i in range(len(numbers) - 1):
-        if numbers[i] >= numbers[i+1]:
-            rep += numbers[i]
+    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                    'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    prev_value = 0
+    for char in reversed(roman_string):
+        value = roman_values[char]
+        if value < prev_value:
+            total -= value
         else:
-            rep -= numbers[i]
-
-    return rep
+            total += value
+        prev_value = value
+    return total
