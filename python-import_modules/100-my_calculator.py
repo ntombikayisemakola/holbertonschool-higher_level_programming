19239be60f5cf04ab7e377ac498e4df1a0726802
Ntombikayise Kidibone Makola <ntombikayise.makola@gmail.com>
2024-05-14 12:04:26 +0000

    6. Build my own calculator!

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -12,13 +12,16 @@ if __name__ == "__main__":
         '*': mul,
         '/': div
     }
-    if argv[2] in ops:
-        num1 = int(argv[1])
-        num2 = int(argv[3])
-        op = ops[argv[2]]
-        result = op(num1, num2)
-        print('{:d} {:s} {:d} = {:d}'.format(num1, argv[2], num2, result))
-    else:
+    if argv[2] not in ops:
         print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
+    try:
+        num1 = int(argv[1])
+        num2 = int(argv[3])
+    except ValueError:
+        print('Usage: {} <a> <operator> <b>'.format(argv[0]))
+        exit(1)
+    op = ops[argv[2]]
+    result = op(num1, num2)
+    print('{:d} {:s} {:d} = {:d}'.format(num1, argv[2], num2, result))
     exit(0)
