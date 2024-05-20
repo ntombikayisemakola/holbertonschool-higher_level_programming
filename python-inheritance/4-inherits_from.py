21591a0b0c46cd54c6cab300834f2b9ee4547ba4
Ntombikayise Kidibone Makola <ntombikayise.makola@gmail.com>
2024-05-20 12:33:33 +0000

    4. Only sub class of

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,16 +3,18 @@
 Contains the inherits_from function.
 """
 
+
 def inherits_from(obj, a_class):
     """
-    Checks if obj is an instance of a class that inherited (directly or indirectly) from the specified class.
+    Checks if obj is an instance of a class that inherited (directly or 
+    indirectly) from the specified class.
 
     Args:
         obj: The object to check.
         a_class: The class to check against.
 
     Returns:
-        True if obj is an instance of a class that inherited from a_class, but is not an instance of a_class itself.
-        False otherwise.
+        True if obj is an instance of a class that inherited from a_class,
+        but is not an instance of a_class itself. False otherwise.
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
