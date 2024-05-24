f588c8a31a7236747149a91cfa3e872c49e77639
Ntombikayise Kidibone Makola <ntombikayise.makola@gmail.com>
2024-05-24 13:00:17 +0000

    0. Abstract Animal Class and its Subclasses

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,15 +1,19 @@
+#!/usr/bin/python3
+"""Module for the abstract class"""
 from abc import ABC, abstractmethod
 
 class Animal(ABC):
+    """parent class Animal"""
     @abstractmethod
     def sound(self):
         pass
 
 class Dog(Animal):
+    """class dog inherits from the Animal class"""
     def sound(self):
         return "Bark"
-
+    
 class Cat(Animal):
+    """class Cat inherits from the Animal class"""
     def sound(self):
         return "Meow"
-
