049829574ef639fdf9bf52c224f840acb4ceb670
Ntombikayise Kidibone Makola <ntombikayise.makola@gmail.com>
2024-05-27 08:00:55 +0000

    5. Save Object to a file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Module containing the function save_to_json_file"""
 import json
-import os
 
 
 def save_to_json_file(my_obj, filename):
@@ -11,16 +10,5 @@ def save_to_json_file(my_obj, filename):
         my_obj: Object to write to text file.
         filename (str): name of the file.
     """
-    # Convert set to list for JSON serialization
-    if isinstance(my_obj, set):
-        my_obj = list(my_obj)
-
-    try:
-        with open(filename, 'w', encoding="utf-8") as f:
-            json.dump(my_obj, f)
-    except PermissionError as e:
-        print(f"[PermissionError] {e}")
-
-# Example usage
-data = {1, 2, 3}
-save_to_json_file(data, 'file_7')
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, f)
