1dbf8181d7f3c8315089a08f209faf9e326998ec
Ntombikayise Kidibone Makola <ntombikayise.makola@gmail.com>
2024-05-31 07:19:52 +0000

    4. Develop a Simple API using Python with Flask

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,12 +1,9 @@
+#!/usr/bin/python3 
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-users = {
-    "jane": {"name": "Jane", "age": 28, "city": "Los Angeles"},
-    "john": {"name": "John", "age": 30, "city": "New York"}
-}
-
+users = {}
 
 @app.route("/")
 def home():
@@ -16,9 +13,11 @@ def home():
 def get_data():
     return jsonify(list(users.keys()))
 
+
 @app.route("/status")
 def status():
     return "OK"
+
 
 @app.route("/users/<username>")
 def get_user(username):
