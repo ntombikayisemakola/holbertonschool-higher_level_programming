71291e968ffa43c023b2c389e6e502122188b6aa
Ntombikayise Kidibone Makola <ntombikayise.makola@gmail.com>
2024-06-18 10:12:27 +0000

    4. Cities by states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,35 +1,53 @@
 #!/usr/bin/python3
-
-"""
-    A script that lists all cities from the database hbtn_0e_0_usa
-    Username, password and database names are given as user args
-"""
-
-
 import sys
 import MySQLdb
 
+def list_cities(mysql_username, mysql_password, db_name):
+    try:
+        # Connect to the MySQL database
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=db_name
+        )
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         host='localhost',
-                         port=3306)
+        # Create a cursor object to interact with the database
+        cursor = db.cursor()
 
-    cursor = db.cursor()
+        # Execute the SQL query
+        query = """
+        SELECT cities.id, cities.name, states.name 
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id ASC
+        """
+        cursor.execute(query)
 
-    sql = """SELECT c.id, c.name, s.name
-          FROM states s, cities c
-          WHERE c.state_id = s.id
-          ORDER BY c.id ASC"""
+        # Fetch all the results
+        results = cursor.fetchall()
 
-    cursor.execute(sql)
+        # Print each row in the specified format
+        for row in results:
+            print(row)
 
-    data = cursor.fetchall()
+    except MySQLdb.Error as e:
+        print(f"Error {e}")
+    finally:
+        # Close the cursor and connection
+        if cursor:
+            cursor.close()
+        if db:
+            db.close()
 
-    for row in data:
-        print(row)
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./4-cities_by_state.py <mysql_username> <mysql_password> <db_name>")
+        sys.exit(1)
 
-    cursor.close()
-    db.close()
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    list_cities(mysql_username, mysql_password, db_name)
