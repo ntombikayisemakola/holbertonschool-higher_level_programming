79d6e95d5e07e6cbd7878f8556f1cf079d2e7651
Ntombikayise Kidibone Makola <ntombikayise.makola@gmail.com>
2024-06-18 12:37:31 +0000

    0. Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,36 +1,23 @@
 #!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa sorted in ascending order by
-states.id
+This script lists all states from the
+database `hbtn_0e_0_usa`.
 """
+
 import MySQLdb
-import sys
+from sys import argv
 
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-if __name__ == "__main__":
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    try:
-        conn = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=mysql_username,
-            passwd=mysql_password,
-            db=db_name,
-            charset="utf8"
-        )
-    except MySQLdb.Error as e:
-        print("Error connecting to database: {}".format(e))
-        sys.exit(1)
-
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
 
     for row in rows:
         print(row)
-
-    cur.close()
-    conn.close()
