19141e84a20a16b2e43692ad2433e764772a98de
Ntombikayise Kidibone Makola <ntombikayise.makola@gmail.com>
2024-06-18 09:44:07 +0000

    3. SQL Injection...

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 
 """
-    A script that lists all states from the database hbtn_0e_0_usa
-    starting with capital letter N
-    Username, password and database names are given as user args
+A script that lists all states from the database hbtn_0e_0_usa
+where the name matches the argument and is safe from SQL injection.
+Username, password, database name, and state name are given as user args.
 """
-
 
 import sys
 import MySQLdb
 
-
 if __name__ == '__main__':
+    if len(sys.argv) != 5:
+        print("Usage: {} <mysql username> <mysql password> <database name> <state name searched>".format(sys.argv[0]))
+        sys.exit(1)
+        
     db = MySQLdb.connect(user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3],
@@ -21,11 +23,10 @@ if __name__ == '__main__':
     cursor = db.cursor()
 
     sql = """SELECT * FROM states
-          WHERE name = %s
-          ORDER BY id ASC"""
-
+             WHERE name = %s
+             ORDER BY id ASC"""
+    
     cursor.execute(sql, (sys.argv[4],))
-
     data = cursor.fetchall()
 
     for row in data:
