819c0f2e2a9ca521efc970e43e8227a9e5108f09
Ntombikayise Kidibone Makola <ntombikayise.makola@gmail.com>
2024-06-18 10:01:35 +0000

    4. Cities by states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 
 """
-A script that lists all cities from the database hbtn_0e_4_usa.
-Username, password, and database name are given as user args.
+    A script that lists all cities from the database hbtn_0e_0_usa
+    Username, password and database names are given as user args
 """
+
 
 import sys
 import MySQLdb
 
+
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>".format(sys.argv[0]))
-        sys.exit(1)
-        
     db = MySQLdb.connect(user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3],
@@ -21,12 +19,13 @@ if __name__ == '__main__':
 
     cursor = db.cursor()
 
-    sql = """SELECT cities.id, cities.name, states.name
-             FROM cities
-             JOIN states ON cities.state_id = states.id
-             ORDER BY cities.id ASC"""
-    
+    sql = """SELECT c.id, c.name, s.name
+          FROM states s, cities c
+          WHERE c.state_id = s.id
+          ORDER BY c.id ASC"""
+
     cursor.execute(sql)
+
     data = cursor.fetchall()
 
     for row in data:
