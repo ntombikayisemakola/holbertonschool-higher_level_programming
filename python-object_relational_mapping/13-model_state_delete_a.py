864585235253d1e9aee56c6a3a1d8d764ddf4c55
Ntombikayise Kidibone Makola <ntombikayise.makola@gmail.com>
2024-06-20 07:10:38 +0000

    13. Delete states

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,46 +1,29 @@
 #!/usr/bin/python3
 """
-Script to delete all State objects with a name containing the letter 'a'
-from the database hbtn_0e_6_usa.
-
-Arguments:
-    mysql username (str)
-    mysql password (str)
-    database name (str)
+This script deletes all State objects
+with a name containing the letter `a`
+from the database `hbtn_0e_6_usa`.
 """
 
-import sys
+from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == "__main__":
-    # Get MySQL credentials and database name from command line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
+    """
+    Deletes State objects on the database.
+    """
 
-    # Connect to the MySQL server using SQLAlchemy
-    engine = create_engine(f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{db_name}')
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
+    Session = sessionmaker(bind=engine)
 
-    # Bind the engine to the metadata of the Base class so that the declaratives can be accessed through a DBSession instance
-    Base.metadata.bind = engine
+    session = Session()
 
-    # Create a session maker instance
-    DBSession = sessionmaker(bind=engine)
+    for instance in session.query(State).filter(State.name.contains('a')):
+        session.delete(instance)
 
-    # Create a session
-    session = DBSession()
-
-    # Query for all State objects whose name contains the letter 'a'
-    states_with_a = session.query(State).filter(State.name.like('%a%')).all()
-
-    # Delete each State object found
-    for state in states_with_a:
-        session.delete(state)
-
-    # Commit the transaction to delete the objects from the database
     session.commit()
-
-    # Close the session
     session.close()
